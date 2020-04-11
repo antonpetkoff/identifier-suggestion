f32054828e643a99635af05535acc44453abb2dd
Anton Petkov <anton.petkoff@gmail.com>
2020-04-11 19:07:16 +0300

    Train the baseline model with random seeds set for reproducibility

--- lib/pipelines/baseline.py
+++ lib/pipelines/baseline.py
@@ -1,15 +1,23 @@
 import argparse
 import pandas as pd
 import numpy as np
+import tensorflow as tf
 
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Input, LSTM, Dense
 
-from tqdm import tqdm
-
+# custom imports
 from lib.evaluation.sequence import compute_f1_score
 from lib.preprocessing.tokens import tokenize_method_body, get_subtokens
 
+# set random seeds for reproducible results
+from numpy.random import seed
+seed(1)
+# from tensorflow.random import set_seed
+tf.random.set_seed(1) # set TensorFlow's global seed
+
+# import tqdm and enable it for pandas for progress_apply
+from tqdm import tqdm
 tqdm.pandas()
 
 parser = argparse.ArgumentParser(description='Baseline Seq2Seq model')
@@ -35,27 +43,29 @@ parser.add_argument('--batch_size', type=int, help='Batch Size', required=True)
 parser.add_argument('--random_seed', type=int, help='Random Seed', required=True)
 
 
-SEQ_START = '\t'
-SEQ_END = '\n'
-
+SEQ_START_TOKEN = '\t'
+SEQ_END_TOKEN = '\n'
+PAD_TOKEN = ' '
 
 def seq2seq(args, input_texts, target_texts):
     # Vectorize the data.
-    input_vocabulary = set()
-    target_vocabulary = set()
+    # add the PAD_TOKEN to both vocabularies in order to pad the sequences
+    input_vocabulary = set(PAD_TOKEN)
+    target_vocabulary = set(PAD_TOKEN)
 
     for input_text, target_text in zip(input_texts, target_texts):
         # We use "tab" as the "start sequence" character
         # for the targets, and "\n" as "end sequence" character.
-        target_text = [SEQ_START] + target_text + [SEQ_END]
+        target_text = [SEQ_START_TOKEN] + target_text + [SEQ_END_TOKEN]
 
         for token in input_text:
-            # ignore string literals, because they increase the vocabulary size too much
-            if not token.startswith('"') and token not in input_vocabulary:
+            if token not in input_vocabulary:
                 input_vocabulary.add(token)
         for token in target_text:
             if token not in target_vocabulary:
                 target_vocabulary.add(token)
+
+    # compute core hyperparameters of the model
 
     input_vocabulary = sorted(list(input_vocabulary))
     target_vocabulary = sorted(list(target_vocabulary))
@@ -80,8 +90,7 @@ def seq2seq(args, input_texts, target_texts):
         [(token, i) for i, token in enumerate(target_vocabulary)]
     )
 
-    print(target_token_index)
-
+    # allocate tensors in the format accepted by the model for the training data
     encoder_input_data = np.zeros(
         (len(input_texts), encoder_seq_length, num_encoder_tokens),
         dtype='float32'
@@ -95,6 +104,71 @@ def seq2seq(args, input_texts, target_texts):
         dtype='float32'
     )
 
+    # encode the training data into tensors of numbers for the model
+    # i is for sample_id
+    for i, (input_text, target_text) in enumerate(zip(input_texts, target_texts)):
+        # process inputs...
+        # t is for timestemp
+        # take at most the first N tokens where N = encoder_seq_length
+        for t, token in enumerate(input_text[:encoder_seq_length]):
+            encoder_input_data[i, t, input_token_index[token]] = 1.
+
+        # pad any remaining placeholders with the PAD_TOKEN
+        encoder_input_data[i, t + 1:, input_token_index[PAD_TOKEN]] = 1.
+
+        # process targets...
+        # take at most the first N tokens where N = decoder_seq_length
+        for t, token in enumerate(target_text[:decoder_seq_length]):
+            # decoder_target_data is ahead of decoder_input_data by one timestep
+            decoder_input_data[i, t, target_token_index[token]] = 1.
+            if t > 0:
+                # decoder_target_data will be ahead by one timestep
+                # and will not include the start character.
+                decoder_target_data[i, t - 1, target_token_index[token]] = 1.
+
+        # pad any remaining placeholders with the PAD_TOKEN
+        decoder_input_data[i, t + 1:, target_token_index[PAD_TOKEN]] = 1.
+        decoder_target_data[i, t:, target_token_index[PAD_TOKEN]] = 1.
+
+    # Define the Seq2Seq model in Keras
+    # Define an input sequence and process it.
+    encoder_inputs = Input(shape=(None, num_encoder_tokens))
+    encoder = LSTM(args.latent_dim, return_state=True)
+    encoder_outputs, state_h, state_c = encoder(encoder_inputs)
+    # We discard `encoder_outputs` and only keep the states.
+    encoder_states = [state_h, state_c]
+
+    # Set up the decoder, using `encoder_states` as initial state.
+    decoder_inputs = Input(shape=(None, num_decoder_tokens))
+    # We set up our decoder to return full output sequences,
+    # and to return internal states as well. We don't use the
+    # return states in the training model, but we will use them in inference.
+    decoder_lstm = LSTM(args.latent_dim, return_sequences=True, return_state=True)
+    decoder_outputs, _, _ = decoder_lstm(decoder_inputs, initial_state=encoder_states)
+    decoder_dense = Dense(num_decoder_tokens, activation='softmax')
+    decoder_outputs = decoder_dense(decoder_outputs)
+
+    # Define the model that will turn
+    # `encoder_input_data` & `decoder_input_data` into `decoder_target_data`
+    model = Model([encoder_inputs, decoder_inputs], decoder_outputs)
+
+    # Run training
+    model.compile(
+        optimizer='rmsprop',
+        loss='categorical_crossentropy',
+        metrics=['accuracy']
+    )
+    model.fit(
+        [encoder_input_data, decoder_input_data],
+        decoder_target_data,
+        batch_size=args.batch_size,
+        epochs=args.epochs,
+        validation_split=0.2
+    )
+
+
+def filter_out_string_literals(seq):
+    return [token for token in seq if not token.startswith('"')]
 
 def main():
     args = parser.parse_args()
@@ -104,7 +178,9 @@ def main():
     print(f'loaded dataset of size {len(df)}')
 
     # dataset
-    df['body_tokens'] = df['body'].progress_apply(tokenize_method_body)
+    df['body_tokens'] = df['body'] \
+        .progress_apply(tokenize_method_body) \
+        .progress_apply(filter_out_string_literals) # ignore string literals, because they increase the vocabulary size too much
     input_texts = df[df.body_tokens.str.len() > 0]['body_tokens'] # remove invalid methods which cannot be parsed
 
     # the output sequences are marked with a <start> and <end> special tokens
