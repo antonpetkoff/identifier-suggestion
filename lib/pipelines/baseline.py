7655c75c80e20f19da9971b66ab7e327dec86022
Anton Petkov <anton.petkoff@gmail.com>
2020-04-11 18:38:20 +0300

    Start adapting a simple Keras Seq2Seq example as a baseline

--- lib/pipelines/baseline.py
+++ lib/pipelines/baseline.py
@@ -1,5 +1,9 @@
 import argparse
 import pandas as pd
+import numpy as np
+
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Input, LSTM, Dense
 
 from tqdm import tqdm
 
@@ -15,6 +19,8 @@ parser.add_argument('--file_data_raw', type=str, help='Raw data file', required=
 
 # hyper parameters
 parser.add_argument('--max_input_length', type=int, help='Max input sequence length', required=True)
+
+# TODO: should we include <start> and <end>? we will add the <start> and <end> tokens which will effectively increase the seq length to 10 during training and prediction
 parser.add_argument('--max_output_length', type=int, help='Max output sequence length', required=True)
 parser.add_argument('--input_vocab_size', type=int, help='Input vocabulary size', required=True)
 parser.add_argument('--input_embedding_dim', type=int, help='Input embedding dimensionality', required=True)
@@ -29,6 +35,67 @@ parser.add_argument('--batch_size', type=int, help='Batch Size', required=True)
 parser.add_argument('--random_seed', type=int, help='Random Seed', required=True)
 
 
+SEQ_START = '\t'
+SEQ_END = '\n'
+
+
+def seq2seq(args, input_texts, target_texts):
+    # Vectorize the data.
+    input_vocabulary = set()
+    target_vocabulary = set()
+
+    for input_text, target_text in zip(input_texts, target_texts):
+        # We use "tab" as the "start sequence" character
+        # for the targets, and "\n" as "end sequence" character.
+        target_text = [SEQ_START] + target_text + [SEQ_END]
+
+        for token in input_text:
+            # ignore string literals, because they increase the vocabulary size too much
+            if not token.startswith('"') and token not in input_vocabulary:
+                input_vocabulary.add(token)
+        for token in target_text:
+            if token not in target_vocabulary:
+                target_vocabulary.add(token)
+
+    input_vocabulary = sorted(list(input_vocabulary))
+    target_vocabulary = sorted(list(target_vocabulary))
+    num_encoder_tokens = len(input_vocabulary)
+    num_decoder_tokens = len(target_vocabulary)
+    max_encoder_seq_length = max([len(txt) for txt in input_texts])
+    max_decoder_seq_length = max([len(txt) for txt in target_texts])
+
+    print('Number of samples:', len(input_texts))
+    print('Number of unique input tokens:', num_encoder_tokens)
+    print('Number of unique output tokens:', num_decoder_tokens)
+
+    print('Max sequence length for inputs:', max_encoder_seq_length)
+    print('Max sequence length for outputs:', max_decoder_seq_length)
+    encoder_seq_length = min(max_encoder_seq_length, args.max_input_length)
+    decoder_seq_length = min(max_decoder_seq_length, args.max_output_length)
+
+    input_token_index = dict(
+        [(token, i) for i, token in enumerate(input_vocabulary)]
+    )
+    target_token_index = dict(
+        [(token, i) for i, token in enumerate(target_vocabulary)]
+    )
+
+    print(target_token_index)
+
+    encoder_input_data = np.zeros(
+        (len(input_texts), encoder_seq_length, num_encoder_tokens),
+        dtype='float32'
+    )
+    decoder_input_data = np.zeros(
+        (len(input_texts), decoder_seq_length, num_decoder_tokens),
+        dtype='float32'
+    )
+    decoder_target_data = np.zeros(
+        (len(input_texts), decoder_seq_length, num_decoder_tokens),
+        dtype='float32'
+    )
+
+
 def main():
     args = parser.parse_args()
     #print(args) # TODO: persist configuration in experiment folter
@@ -38,18 +105,15 @@ def main():
 
     # dataset
     df['body_tokens'] = df['body'].progress_apply(tokenize_method_body)
+    input_texts = df[df.body_tokens.str.len() > 0]['body_tokens'] # remove invalid methods which cannot be parsed
 
     # the output sequences are marked with a <start> and <end> special tokens
-    df['method_name_subtokens'] = df['method_name'].progress_apply(get_subtokens)
-    df_clean = df[df.body_tokens.str.len() > 0] # remove invalid methods which cannot be parsed
+    target_texts = df['method_name'].progress_apply(get_subtokens)
 
-    print(df.head(5))
-
-    # TODO: preprocess data from raw to model format?
-    # TODO: use data iterator
-
-    f1_score = compute_f1_score('transformSearchResponse', 'modifySearchResponseData')
-    print(f'F1 score is {f1_score}')
+    # print(input_texts.head(5))
+    # print('-------')
+    # print(target_texts.head(5))
+    seq2seq(args, input_texts, target_texts)
 
 
 if __name__ == "__main__":
